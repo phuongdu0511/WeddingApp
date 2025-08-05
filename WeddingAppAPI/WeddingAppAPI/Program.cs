c262fae76b90953abb24466d859300bb28ed4334
phuongduy0511 <duongphuongduy1996@gmail.com>
2025-08-05 20:04:36 +0700

    add CORS origin and change gitignore

--- WeddingAppAPI/WeddingAppAPI/Program.cs
+++ WeddingAppAPI/WeddingAppAPI/Program.cs
@@ -14,6 +14,15 @@ builder.Services.AddSwaggerGen();
 // Configuration Extension
 builder.Services.AddSqlConfiguration(builder.Configuration);
 builder.Services.AddServiceConfiguration();
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
 
 var app = builder.Build();
 
@@ -23,6 +32,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 

cab353cf121001d9e4815a20c227228e147c1e02
phuongduy0511 <duongphuongduy1996@gmail.com>
2025-10-07 23:39:41 +0700

    before zip image

--- WeddingAppUI/wedding-app/src/main.tsx
+++ WeddingAppUI/wedding-app/src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import '@fontsource/lora';
-import '@fontsource/great-vibes';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
